1388040fe470c97fd25f09949ad41d118073314e
Louka <95499931+Grimmtroupe@users.noreply.github.com>
2024-12-05 15:12:59 -0500

    user

--- wwwroot/Posts/js/API_User.js
+++ wwwroot/Posts/js/API_User.js
@@ -1,41 +1,41 @@
 //const API_URL = "https://api-server-5.glitch.me/api/contacts";
-const API_URL = "http://localhost:5000/api/contacts";
+const API_URL = "http://localhost:5000/api/user";
 let currentHttpError = "";
 
 function API_getcurrentHttpError () {
     return currentHttpError; 
 }
-function API_GetContacts() {
+function API_GetUser() {
     return new Promise(resolve => {
         $.ajax({
             url: API_URL,
-            success: contacts => { currentHttpError = ""; resolve(contacts); },
+            success: users => { currentHttpError = ""; resolve(users); },
             error: (xhr) => { console.log(xhr); resolve(null); }
         });
     });
 }
-function API_GetContact(contactId) {
+function API_GetUser(userId) {
     return new Promise(resolve => {
         $.ajax({
-            url: API_URL + "/" + contactId,
-            success: contact => { currentHttpError = ""; resolve(contact); },
+            url: API_URL + "/" + userId,
+            success: user => { currentHttpError = ""; resolve(user); },
             error: (xhr) => { currentHttpError = xhr.responseJSON.error_description; resolve(null); }
         });
     });
 }
-function API_SaveContact(contact, create) {
+function API_SaveUser(user, create) {
     return new Promise(resolve => {
         $.ajax({
-            url: create ? API_URL :  API_URL + "/" + contact.Id,
+            url: create ? API_URL :  API_URL + "/" + user.Id,
             type: create ? "POST" : "PUT",
             contentType: 'application/json',
-            data: JSON.stringify(contact),
+            data: JSON.stringify(user),
             success: (/*data*/) => { currentHttpError = ""; resolve(true); },
             error: (xhr) => {currentHttpError = xhr.responseJSON.error_description; resolve(false /*xhr.status*/); }
         });
     });
 }
-function API_DeleteContact(id) {
+function API_DeleteUser(id) {
     return new Promise(resolve => {
         $.ajax({
             url: API_URL + "/" + id,
